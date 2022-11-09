3ddfda9ddeeeb60d6c194b63bfaea6500af84e61
Stas <finneganlucky@gmail.com>
2022-11-09 14:38:30 +0100

    main

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import {MotiView} from "moti";
+import {Easing} from "react-native-reanimated";
+
+const _color = '#64eb82'
+const _size = 100
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.dot, styles.center]}>
+        {[...Array(4).keys()].map(index =>{
+          return (
+              <MotiView
+                  from={{opacity: 0.5, scale: 1}}
+                  animate={{opacity: 0, scale: 4}}
+                  transition={{
+                    type: 'timing',
+                    duration: 2000,
+                    easing: Easing.out(Easing.ease),
+                    delay: index * 400,
+                    repeatReverse: false,
+                    loop: true,
+                  }}
+                  key={index}
+                  style={[
+                      StyleSheet.absoluteFillObject,
+                      styles.dot
+                  ]}
+              />)
+        })}
+        <Feather name='phone-outgoing' size={32} color='#fff'/>
+      </View>
     </View>
   );
 }
@@ -13,8 +40,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dot: {
+    width: _size,
+    height: _size,
+    borderRadius: _size,
+    backgroundColor: _color
+  }
 });
